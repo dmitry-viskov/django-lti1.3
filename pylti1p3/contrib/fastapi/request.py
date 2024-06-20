0d9620c6506631212c63f4c4835c1ae3034b32dd
heindrickdumdum <heindrickd17@gmail.com>
2024-06-20 04:14:13 -0400

    feat: Update FastAPI request

--- pylti1p3/contrib/fastapi/request.py
+++ pylti1p3/contrib/fastapi/request.py
@@ -2,20 +2,31 @@ from pylti1p3.request import Request
 
 
 class FastAPIRequest(Request):
-    _session = None
+    _request = None
+    _form_data = None
 
-    def __init__(self, request):
+    def __init__(self, request, form_data=None):
+        """
+        Parameters:
+            request: FastAPI request
+            form_data: form data from FastAPI request
+                To get form data from FastAPI request, must use async method.
+                As we don't use async functions here, form data must be provided from outside.
+        """
+
         super().__init__()
+
         self._request = request
+        self._form_data = form_data or {}
 
     @property
     def session(self):
         return self._request.session
 
-    async def get_param(self, key):
+    def get_param(self, key):
         if self._request.method == "GET":
             return self._request.query_params.get(key, None)
-        return (await self._request.form()).get(key)
+        return self._form_data.get(key)
 
     def get_cookie(self, key):
         return self._request.cookies.get(key, None)
